480d21d902f8c8ec0d1f13e16df4d71b9ca32176
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2021-07-02 11:46:08 +0100

    BoulderDashStart

---
+++ 19932_GabrielGomes_19921_VascoGomes_TPO2_PO2_2020-2021/src/pt/ipbeja/po2/boulderdash/gui/BoulderDashStart.java
@@ -0,0 +1,15 @@
+package pt.ipbeja.po2.boulderdash.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Vasco Gomes 19921
+ * @date 02/07/2021
+ */
+public class BoulderDashStart extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
